6537bda6e43f598862948f1261e37a74df935987
Desmond <desmondfon@yahoo.com>
2022-10-27 01:41:27 +0100

    fitst commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', generateAdvice);
+
+function generateAdvice() {
+    console.log('click');
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.adviceslip.com/advice', true);
+
+  xhr.onload = function () {
+    if (this.status >= 200 && this.status < 300) {
+      const advice = JSON.parse(this.responseText);
+      const adviceText = document.getElementById('advice');
+      adviceText.innerText = JSON.stringify(advice.slip.advice);
+      const adviceID = document.getElementById('adviceID');
+      adviceID.innerText = JSON.stringify(advice.slip.id);
+  }
+} 
+
+xhr.send();
+};  
