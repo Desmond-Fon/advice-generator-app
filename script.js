d16b0a0d3043f99bbb0a8637a4d4d70b091d1850
Desmond <desmondfon@yahoo.com>
2022-10-27 02:05:11 +0100

    new api call method added

--- script.js
+++ script.js
@@ -2,20 +2,40 @@ const btn = document.getElementById('btn');
 
 btn.addEventListener('click', generateAdvice);
 
-function generateAdvice() {
-    // console.log('click');
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.adviceslip.com/advice', true);
 
-  xhr.onload = function () {
-    if (this.status >= 200 && this.status < 300) {
-      const advice = JSON.parse(this.responseText);
+//-----usingAJAX-----
+
+// function generateAdvice() {
+//     // console.log('click');
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'https://api.adviceslip.com/advice', true);
+
+//   xhr.onload = function () {
+//     if (this.status >= 200 && this.status < 300) {
+//       const advice = JSON.parse(this.responseText);
+//       const adviceText = document.getElementById('advice');
+//       adviceText.innerText = JSON.stringify(advice.slip.advice);
+//       const adviceID = document.getElementById('adviceID');
+//       adviceID.innerText = JSON.stringify(advice.slip.id);
+//   }
+// } 
+
+// xhr.send();
+// };  
+
+
+//-------using promise---------
+function generateAdvice() { 
+
+  fetch('https://api.adviceslip.com/advice')
+  .then(res => res.json())
+   .then(data => {
+    // console.log(data);
       const adviceText = document.getElementById('advice');
-      adviceText.innerText = JSON.stringify(advice.slip.advice);
+      adviceText.innerText = JSON.stringify(data.slip.advice);
       const adviceID = document.getElementById('adviceID');
-      adviceID.innerText = JSON.stringify(advice.slip.id);
-  }
-} 
+      adviceID.innerText = JSON.stringify(data.slip.id);
+   })
+   .catch(err => console.log(err));
 
-xhr.send();
-};  
+}
